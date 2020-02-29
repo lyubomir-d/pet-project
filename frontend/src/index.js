8b76d20bf036aec8ee07971b9505f446bff7b685
Lyubomir Dolgopolov <lyubomir.dolgopolov@gmail.com>
2020-02-29 21:56:00 +0300

    Fixed bug with routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, HashRouter as Router } from "react-router-dom";
 import "regenerator-runtime";
 import { Provider } from "react-redux";
 import store from "./store/store";
